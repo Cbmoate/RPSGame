873eab994ab985cc0d5f60cc0dbf160b86256af6
CB <cbmoate@gmail.com>
2015-12-07 21:10:43 -0500

    number one, added JS stub and HTML stub

---
+++ js/cutter.js
@@ -0,0 +1,15 @@
+var choices = ['rock', 'paper', 'scissors'];
+var rando = Math.floor(Math.random() * choices.length);
+
+var rounds = 0;
+var wins = 0;
+var loses = 0;
+
+$('#rounds').html(rounds);
+$('#yourScore').html(wins);
+$('#computerScore').html(loses);
+
+
+$('#rounds').html(rounds)
+$('#yourScore').html(wins)
+$('#computerScore').html(loses)
