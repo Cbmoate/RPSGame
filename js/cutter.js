3777f6cc011a22c7add7f35e900576aa0c814f22
CB <cbmoate@gmail.com>
2015-12-09 21:34:30 -0500

    modal shows, buttons return things.  almost there

--- js/cutter.js
+++ js/cutter.js
@@ -1,54 +1,63 @@
 $( document ).ready(function() {
+   //computer choice logic
+  function computerChoice(){
+    var choices = ['rock', 'paper', 'scissors'];
+    var computerRando = choices[Math.floor(Math.random()* choices.length)];
+    return computerRando;
+  } 
 
-  $("#rock").click(function(){
-    alert("rock");
-    });
-  $("#paper").click(function(){
-    alert("paper");
-    }); 
-  $("#scissors").click(function(){
-    alert("scissors");
-    }); 
-
-  var choices = ['rock', 'paper', 'scissors'];
-
-  //computer choice logic
-  var computerChoice = Math.random();
-  if (computerChoice < 0.34) {
-      computerChoice = "rock";
-  } else if (computerChoice > 0.67) {
-      computerChoice = "paper";
-  } else {
-      computerChoice = "scissors";
+  function choose(userChoice){
+    if (userChoice === "rock"){
+      console.log(userChoice)
+      return "rock";
+    } else if(userChoice === "paper"){
+      return "paper";
+    } else if(userChoice === "scissors"){
+      return "scissors";
+    }
   }
 
-  //User Choice
+  $("#rock").click(function(){
+    choose("rock");
+    });
+  $("#paper").click(function(){
+    choose("paper");
+    }); 
+  $("#scissors").click(function(){
+    choose("scissors");
+    }); 
+  $("#go").click(function(){
+    compare(choose, computerChoice);
+  $("#bigWin").modal('show')
+  });
+
+  //fire off game
   var compare = function(choice1, choice2) {
     if (choice1 === choice2) {
-      document.getElementById("result").innerHTML = "Tie!";
+      document.getElementById("result").innerHTML = "Tie!  Train harder!";
     }
     else if (choice1 === "rock") {
       if (choice2 === "scissors") {
-        document.getElementById("result").innerHTML = "You Win!";
+        document.getElementById("result").innerHTML = "You have vanquished your enemy!";
       }
       else {
-        document.getElementById("result").innerHTML = "You Lose!";
+        document.getElementById("result").innerHTML = "You have brought shame upon your dojo!!";
       }
     }
     else if (choice1 === "paper") {
       if (choice2 === "rock") {
-        document.getElementById("result").innerHTML = "You Win!";
+        document.getElementById("result").innerHTML = "You have vanquished your enemy!";
       }
       else if (choice2 ==="scissors") {
-        document.getElementById("result").innerHTML = "You Lose!";
+        document.getElementById("result").innerHTML = "You have brought shame upon your dojo!!";
       }
     }
     else if (choice1 === "scissors") {
       if (choice2 === "rock") {
-        document.getElementById("result").innerHTML = "You Lose!";
+        document.getElementById("result").innerHTML = "You have brought shame upon your dojo!!";
       }
       else if (choice2 === "paper") {
-        document.getElementById("result").innerHTML = "You Win!";
+        document.getElementById("result").innerHTML = "You have vanquished your enemy!";
       }
     }
     else {
